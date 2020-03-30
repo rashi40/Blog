533fc0d8783f5fb97221b51de515a089f0930bfc
rashi40 <gungunnegi40@gmail.com>
2020-03-30 16:04:56 +0000

    First commit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var app= express();
+var expressSanitizer= require("express-sanitizer");
+var methodOverride = require("method-override");
 var bodyParser =require("body-parser");
 var mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(methodOverride("_method"));
 mongoose.connect("mongodb://localhost:27017/restful_Blog_app",{useNewUrlParser:true});
  
 app.use(bodyParser.urlencoded({extended: true }));
+app.use(expressSanitizer());
 var blogSchema =new mongoose.Schema({
 	title: String,
 	image: String,
@@ -45,6 +48,7 @@ app.get("/blogs/new", function(req, res){
 });
 
 app.post("/blogs", function(req, res){
+	req.body.blog.body= req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){
 			res.render("new");
@@ -55,6 +59,52 @@ app.post("/blogs", function(req, res){
 	});
 });
 
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else
+			{
+				res.render("show", {blog: foundBlog});
+			}
+	});
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+		req.redirect("/blogs");	
+		}
+		else {
+				res.render("edit", {blog: foundBlog});
+		}
+	});
+	
+});
+
+app.put("/blogs/:id", function(req, res){
+		req.body.blog.body= req.sanitize(req.body.blog.body);
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else{
+			res.redirect("/blogs/"+req.params.id);
+		}
+	});
+});
+
+app.delete("/blogs/:id", function(req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else {
+			res.redirect("/blogs");
+		}
+	});
+});
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Server is Running!");
 });
